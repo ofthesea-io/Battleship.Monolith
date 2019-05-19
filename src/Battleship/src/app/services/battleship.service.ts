b120e6126e0234a6fcd399c0330a585969560c5c
Jakes <jakes.signeto@hotmail.com>
2019-05-19 15:50:11 +0100

    Test: Minor change

--- src/Battleship/src/app/services/battleship.service.ts
+++ src/Battleship/src/app/services/battleship.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Subject } from "rxjs/Subject";
