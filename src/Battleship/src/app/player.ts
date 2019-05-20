743da963c243277cabf06bc524b5af66693a2ccf
jakespotgieter <jakespotgieter@gmail.com>
2019-05-20 17:51:53 +0100

    Refactoring

--- src/Battleship/src/app/player.ts
+++
@@ -1,5 +0,0 @@
-export interface Player {
-  firstname: string;
-  lastname: string;
-  numberOfShips: number;
-}
--- src/Battleship/src/app/playerstatistics.ts
+++
@@ -1,6 +0,0 @@
-export interface PlayerStatistics {
-  hit: number;
-  miss: number;
-  sunk: number;
-  status: boolean;
-}
